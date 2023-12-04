a555e719f7329e809c3be2864bda4215fdb90ff2
ken <ken789030@gmail.com>
2023-12-04 08:54:29 +0800

    refactor:test add function setUp , scoreShouldBe

--- tests/TennisTest.php
+++ tests/TennisTest.php
@@ -7,10 +7,21 @@ use PHPUnit\Framework\TestCase;
 
 class TennisTest extends TestCase
 {
+    private Tennis $tennis;
+
     public function test_Love_All()
     {
-        $tennis = new Tennis();
-        $score = $tennis->score();
-        $this->assertEquals('Love All', $score);
+        $this->scoreShouldBe('Love All');
     }
+
+    private function scoreShouldBe($expected): void
+    {
+        $this->assertEquals($expected, $this->tennis->score());
+    }
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->tennis = new Tennis();
+    }
+
 }
