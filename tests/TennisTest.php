ba1e25212596f1161db63cc4671bd1e1c2383937
ken <ken789030@gmail.com>
2023-12-06 20:00:28 +0800

    refactor

--- tests/TennisTest.php
+++ tests/TennisTest.php
@@ -38,14 +38,13 @@ class TennisTest extends TestCase
 
     public function test_Love_Fifteen()
     {
-        $this->tennis->addSecondPlayerScore();
+        $this->givenSecondPlayerScoreTimes(1);
         $this->scoreShouldBe('Love Fifteen');
     }
 
     public function test_Love_Thirty()
     {
-        $this->tennis->addSecondPlayerScore();
-        $this->tennis->addSecondPlayerScore();
+        $this->givenSecondPlayerScoreTimes(2);
         $this->scoreShouldBe('Love Thirty');
     }
 
@@ -65,6 +64,16 @@ class TennisTest extends TestCase
     {
         for ($i = 0; $i < $times; $i++) {
             $this->tennis->addFirstPlayerScore();
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function givenSecondPlayerScoreTimes(int $times): void
+    {
+        for ($i = 0; $i < $times; $i++) {
+            $this->tennis->addSecondPlayerScore();
         }
     }
 
