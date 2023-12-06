95617fde2fdfafd43313ea08b1393dcd9f31a504
ken <ken789030@gmail.com>
2023-12-06 19:59:26 +0800

    refactor

--- src/Tennis.php
+++ src/Tennis.php
@@ -10,6 +10,7 @@ class Tennis
      * @var array|string[]
      */
     private array $mappingScoreName = [
+        0 => 'Love',
         1 => 'Fifteen',
         2 => 'Thirty',
         3 => 'Forty',
@@ -22,17 +23,9 @@ class Tennis
 
     public function score()
     {
-        if ($this->secondPlayerScore === 1) {
 
-            return 'Love Fifteen';
-        }
-
-        if ($this->secondPlayerScore === 2) {
-            return 'Love Thirty';
-        }
-
-        if ($this->firstPlayerScore > 0) {
-            return $this->mappingScoreName[$this->firstPlayerScore] . ' Love';
+        if ($this->firstPlayerScore != $this->secondPlayerScore) {
+            return $this->mappingScoreName[$this->firstPlayerScore] . ' ' . $this->mappingScoreName[$this->secondPlayerScore];
         }
 
         return 'Love All';
