f06c3a579bb765bb99e93eaea532201ef1e9ef60
ken <ken789030@gmail.com>
2023-12-12 21:49:34 +0800

    test:Thirty All

--- src/Tennis.php
+++ src/Tennis.php
@@ -27,12 +27,7 @@ class Tennis
             return $this->mappingScoreName[$this->firstPlayerScore] . ' ' . $this->mappingScoreName[$this->secondPlayerScore];
         }
 
-        if ($this->firstPlayerScore === 1) {
-
-            return $this->mappingScoreName[$this->firstPlayerScore] . ' All';
-        }
-
-        return 'Love All';
+        return $this->mappingScoreName[$this->firstPlayerScore] . ' All';
     }
 
     public function addFirstPlayerScore()
