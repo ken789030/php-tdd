ed7b5d561b28c34dcbc96013b11497ad2473a218
ken <ken789030@gmail.com>
2023-12-12 08:59:33 +0800

    test:all single point

--- src/Bowling.php
+++ src/Bowling.php
@@ -5,12 +5,21 @@ namespace App;
 class Bowling
 {
 
+    private array $rolls = [];
+
     public function __construct()
     {
     }
 
     public function score()
     {
-        return 0;
+        return array_sum($this->rolls);
+    }
+
+    public function rollMany(int $rollCount, int $pins)
+    {
+        for ($i = 0; $i < $rollCount; $i++) {
+            $this->rolls[] = $pins;
+        }
     }
 }
