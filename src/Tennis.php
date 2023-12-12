31666f7ff7a37bfaa3033b5e90333e2ebae00c99
ken <ken789030@gmail.com>
2023-12-12 10:47:40 +0800

    test:Fifteen All

--- src/Tennis.php
+++ src/Tennis.php
@@ -27,6 +27,10 @@ class Tennis
             return $this->mappingScoreName[$this->firstPlayerScore] . ' ' . $this->mappingScoreName[$this->secondPlayerScore];
         }
 
+        if ($this->firstPlayerScore === 1) {
+            return 'Fifteen All';
+        }
+
         return 'Love All';
     }
 
