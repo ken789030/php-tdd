0c0d54f214f9595b6814e66cc2fa957a99ccdc8d
ken <ken789030@gmail.com>
2023-12-13 17:35:15 +0800

    test:Love All

---
+++ tests/TennisTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+use App\Tennis;
+use PHPUnit\Framework\TestCase;
+
+class TennisTest extends TestCase
+{
+
+    public function test_Love_All()
+    {
+        $tennis = new Tennis();
+        $score = $tennis->score();
+        self::assertEquals('Love All', $score);
+    }
+}
