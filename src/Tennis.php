3c0bf26fd1b06d5a2a8ffcff7a34011fdce6eadd
ken <ken789030@gmail.com>
2023-12-13 17:58:54 +0800

    refactor

--- src/Tennis.php
+++ src/Tennis.php
@@ -27,22 +27,9 @@ class Tennis
 
     public function score()
     {
-        if ($this->isDiffScore()) {
-            if ($this->firstPlayerScore > 3 || $this->secondPlayerScore > 3) {
-                if (abs($this->firstPlayerScore - $this->secondPlayerScore) === 1) {
-                    return $this->advPlayer() . ' Adv';
-                }
-
-                return $this->advPlayer() . ' Win';
-            }
-            return $this->lookupScore();
-        }
-
-        if ($this->isDeuce()) {
-            return $this->deuce();
-        }
-
-        return $this->sameScore();
+        return $this->isDiffScore()
+            ? ($this->isReadyGamePoint() ? $this->advState() : $this->lookupScore())
+            : ($this->isDeuce() ? $this->deuce() : $this->sameScore());
 
     }
 
@@ -104,5 +91,29 @@ class Tennis
         return $this->firstPlayerScore > $this->secondPlayerScore
             ? $this->firstPlayerName
             : $this->secondPlayerName;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAdv(): bool
+    {
+        return abs($this->firstPlayerScore - $this->secondPlayerScore) === 1;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReadyGamePoint(): bool
+    {
+        return $this->firstPlayerScore > 3 || $this->secondPlayerScore > 3;
+    }
+
+    /**
+     * @return string
+     */
+    public function advState(): string
+    {
+        return $this->isAdv() ? $this->advPlayer() . ' Adv' : $this->advPlayer() . ' Win';
     }
 }
