6163433a34f1e50258f232e440294b01f87c22e4
ken <ken789030@gmail.com>
2023-12-15 21:03:06 +0800

    test:Thirty Love

--- src/Tennis.php
+++ src/Tennis.php
@@ -16,6 +16,11 @@ class Tennis
         if ($this->firstPlayerScore === 1) {
             return 'Fifteen Love';
         }
+
+        if ($this->firstPlayerScore === 2) {
+            return 'Thirty Love';
+        }
+
         return 'Love All';
     }
 
